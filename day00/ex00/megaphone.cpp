645cbb3e1f641dc9607f860d01c6fdb49f869e98
msaliuta <mssalyuta@gmail.com>
2019-11-30 19:23:56 +0200

    d00ex00-ready

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -7,24 +7,25 @@ void	print_out(char *str)
 
 	while (str[++i])
 	{
-		if (str[i] >= 'a' $$ str[i] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 			c = str[i] - 32;
 		else
 			c = str[i];
-		std::cout << str[i];
+		std::cout << c;
 	}
 }
 
 int	main(int ac, char **av)
 {
-	int i;
+	int i = 0;
 
 	if (ac == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
 	}
 	while (av[++i])
-		print_str(av[i]);
+		print_out(av[i]);
 	std::cout << std::endl;
 
 	return (0);
